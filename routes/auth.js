20dfb6c71fbf2a5990fdf37945fb786f46e2ea0b
jairoPonti <jairmponti@gmail.com>
2021-03-09 17:45:02 -0300

    Agregando archivos

---
+++ routes/auth.js
@@ -0,0 +1,26 @@
+// Rutas para autenticar usuarios
+const express = require('express');
+const router = express.Router();
+const { check } = require('express-validator');
+const authController = require('../controllers/authCotroller');
+const auth  = require('../middleware/auth');
+
+// Iniciar sesión
+//Su endpoint es api/auth
+router.post('/', 
+  // [
+    
+  //   check('email', 'Agrega un email válido').isEmail(),
+  //   check('password', 'El password debe ser mínimo de 6 caracteres').isLength({ min: 6})
+  // ],
+  authController.autenticarUsuario
+);
+
+// Obtiene el usuario autenticado
+ router.get('/',
+    auth,
+    authController.usuarioAutenticado
+
+ );
+
+module.exports = router;
