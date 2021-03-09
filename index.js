b5389ed5b791affad01ee8a71251aac40a5b40ba
jairoPonti <jairmponti@gmail.com>
2021-03-09 19:40:49 -0300

    Nuevos cambios

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(cors());
 app.use(express.json({ extended: true}));
 
 // Puerto de la app
-const port = process.env.port || 4000;
+const PORT = process.env.PORT || 4000;
 
 // Importar rutas
 app.use('/api/usuarios', require('./routes/usuarios'));
@@ -29,8 +29,8 @@ app.use('/api/tareas', require('./routes/tareas'));
 // });
 
 // arrancar la app
-app.listen(port, '0.0.0.0', () => {
- console.log(`El servidor está funcionando en el puerto ${port}`);
+app.listen(PORT, '0.0.0.0', () => {
+ console.log(`El servidor está funcionando en el puerto ${PORT}`);
 });
 
 
